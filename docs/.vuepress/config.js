1afd59e9a0699ce3a0054b9d56335dc88266dcdf
yuhao <yuhaoideamake.cn>
2023-02-04 12:36:14 +0800

    doc: 文档初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: "lyh's blog",
+    description: "持续学习，持续成长",
+    markdown: {
+      lineNumbers: true,
+    },
+    themeConfig: {
+      sidebar: [
+        // {
+        //   title: '软件资源',
+        //   children: ['/软件资源/软件资源' ],
+        //   initialOpenGroupIndex: 1 // 可选的, 默认值是 0
+        // }
+      ]
+    },
+  }
